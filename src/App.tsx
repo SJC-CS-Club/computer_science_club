c4349112ee4930996e497d406544be34a5ac5359
Biggie Cheese <Jv3studios@gmail.com>
2025-01-28 12:47:22 -0600

    Finished up styles for schedule section

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
       <Stats />
       <Announce />
       <Staff />
-      {/* <Schedule /> */}
+      <Schedule />
       {/* <Location /> */}
       {/* <Feedback /> */}
       {/* <Footer /> */}
--- src/components/schedule.tsx
+++ src/components/schedule.tsx
@@ -1,5 +1,12 @@
 import "../styles/schedule.scss";
 
+const ScheduleMaps = [
+  ["Week 1 | (mm/dd/yy)", "The title of what we're learning"],
+  ["Week 2 | (mm/dd/yy)", "These are placeholder text"],
+  ["Week 3 | (mm/dd/yy)", "These are placeholder text"],
+  ["Week 4 | (mm/dd/yy)", "These are placeholder text"],
+];
+
 export const Schedule = () => {
   return (
     <section id="fall-schedule">
@@ -15,77 +22,16 @@ export const Schedule = () => {
         <div className="fall-schedule-right-inner" id="fall-agenda">
           Weekly Agenda
         </div>
-        <div id="fs-scroll-wheel" className="fall-schedule-right-inner">
-          <button className="fall-info-buttons" id="week1">
-            Week 1 | (08/27/24)
-            <br />
-            Intro to the Club
-          </button>
-          <button className="fall-info-buttons" id="week2">
-            Week 2 | (09/03/24)
-            <br />
-            Intro to Problem-Solving and Critical Thinking
-          </button>
-          <button className="fall-info-buttons" id="week3">
-            Week 3 | (09/10/24)
-            <br />
-            Intro to Git and GitHub
-          </button>
-          <button className="fall-info-buttons" id="week4">
-            Week 4 | (09/17/24)
-            <br />
-            Intro to Cybersecurity
-          </button>
-          <button className="fall-info-buttons" id="week5">
-            Week 5 | (09/24/24)
-            <br />
-            Python Basics
-          </button>
-          <button className="fall-info-buttons" id="week6">
-            Week 6 | (10/01/24)
-            <br />
-            Intermediate Python Programming
-          </button>
-          <button className="fall-info-buttons" id="week7">
-            Week 7 | (10/08/24)
-            <br />
-            Intro to Software Engineering & Best Practices
-          </button>
-          <button className="fall-info-buttons" id="week8">
-            Week 8 | (10/15/24)
-            <br />
-            Intro to Game Development
-          </button>
-          <button className="fall-info-buttons" id="week9">
-            Week 9 | (10/22/24)
-            <br />
-            Intro to Web Development
-          </button>
-          <button className="fall-info-buttons" id="week10">
-            Week 10 | (10/29/24)
-            <br />
-            Intermediate Web Development
-          </button>
-          <button className="fall-info-buttons" id="week11">
-            Week 11 | (11/05/24)
-            <br />
-            Building a Personal Portfolio (Part 1)
-          </button>
-          <button className="fall-info-buttons" id="week12">
-            Week 12 | (11/12/24)
-            <br />
-            Building a Personal Portfolio (Part 2)
-          </button>
-          <button className="fall-info-buttons" id="week13">
-            Week 13 | (11/19/24)
-            <br />
-            Build a final Project / Coding Competition
-          </button>
-          <button className="fall-info-buttons" id="week14">
-            Week 14 | (12/03/24)
-            <br />
-            Final Presentations & End-of-Year Party
-          </button>
+        <div className="fs-scroll-wheel">
+          {ScheduleMaps.map((element, index) => {
+            return (
+              <button className="fall-info-buttons" key={index}>
+                {element[0]}
+                <br />
+                {element[1]}
+              </button>
+            );
+          })}
         </div>
       </aside>
     </section>
