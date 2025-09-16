7f19e33e9e0eba9ae174e8484017f0a11201f6b9
Biggie Cheese <Jv3studios@gmail.com>
2025-09-16 16:54:17 -0500

    fixed button link padding

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -16,14 +16,21 @@ export const Home = () => {
       <div id="buttons">
         <button id="join-us">
           <a
-            href="https://docs.google.com/forms/d/e/1FAIpQLSc832C9yhhzRTrZr0gqI0PCqrXks_wdle7MvauBNCPiUORg2w/viewform?usp=sf_link"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSefU_sunZawCAmDxtfZaxtofQI4sd_2Wn_mGygNKWQ9xHq2_A/viewform?usp=dialog"
             target="_blank"
+            rel="noopener noreferrer"
+            className="full-button-link"
           >
             Join Us
           </a>
         </button>
         <button id="learn-more">
-          <a href="https://matias.me/nsfw/" target="_blank">
+          <a
+            href="https://matias.me/nsfw/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="full-button-link"
+          >
             Learn More
           </a>
         </button>
