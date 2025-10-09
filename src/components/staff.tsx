70ba2d5f517a81c63d0ebf7649d23cd4331e34da
Biggie Cheese <Jv3studios@gmail.com>
2025-10-08 19:35:02 -0500

    fixed changes with staff section

--- src/components/staff.tsx
+++ src/components/staff.tsx
@@ -1,31 +1,63 @@
 import "../styles/staff.scss";
 import logo from "/assets/images/compsci-logo.png";
 
+interface OfficerProps {
+  officerName: string;
+  title: string;
+  image: string;
+  alt: string;
+  email: string;
+  id: string;
+}
+
+const Officers: OfficerProps[] = [
+  {
+    officerName: "Nathaniel Paz",
+    title: "PRESIDENT",
+    image: logo,
+    alt: "a picture of the president",
+    email: "Paz.n089158@stu.sanjac.edu",
+    id: "president",
+  },
+  {
+    officerName: "Nick Hinojosa",
+    title: "VICE PRESIDENT",
+    image: logo,
+    alt: "a picture of the vice president",
+    email: "Hinojosa.N204794@stu.sanjac.edu",
+    id: "vp",
+  },
+  {
+    officerName: "Emilye Hollins",
+    title: "SGA",
+    image: logo,
+    alt: "a picture of the sga representative",
+    email: "hollins.e949016@stu.sanjac.edu",
+    id: "sga",
+  },
+  {
+    officerName: "Andrew Brown",
+    title: "Social Chair",
+    image: logo,
+    alt: "a picture of the social chair",
+    email: "brown.a147159@stu.sanjac.edu",
+    id: "social",
+  },
+];
+
 export const Staff = () => {
   return (
     <section className="staff-container">
       <div className="staff-image-container">
-        <div className="staff">
-          <h2>VICE PRESIDENT</h2>
-          <figure id="officers" className="vice-pres officer">
-            <img src={logo} alt="" />
-          </figure>
-          <a href="#">example@stu.sanjac.edu</a>
-        </div>
-        <div className="staff president" id="pres">
-          <h2>PRESIDENT</h2>
-          <figure id="officers" className="officer">
-            <img src={logo} alt="" />
-          </figure>
-          <a href="#">example@stu.sanjac.edu</a>
-        </div>
-        <div className="staff sponsor">
-          <h2>SPONSOR</h2>
-          <figure id="officers" className="officer">
-            <img src={logo} alt="" />
-          </figure>
-          <a href="#">example@stu.sanjac.edu</a>
-        </div>
+        {Officers.map((elem) => (
+          <div className="staff" id={elem.id} key={elem.id}>
+            <h2>{elem.title}</h2>
+            <figure className="officer" id="officer">
+              <img src={elem.image} alt={elem.alt} />
+            </figure>
+            <a href={`mailto:${elem.email}`}>{elem.email}</a>
+          </div>
+        ))}
       </div>
       <hr />
       <StaffOfficers />
@@ -38,8 +70,8 @@ export const StaffOfficers = () => {
     <div className="officer-section">
       <h1 id="staff-officer-title">Officers</h1>
       <h3 className="staff-officer-names">
-        President · Vacant | Vice President · Vacant | SGA · Vacant | Secretary ·
-        Vacant | Treasurer · Vacant | Social · Vacant
+        President · Vacant | Vice President · Vacant | SGA · Vacant | Secretary
+        · Vacant | Treasurer · Vacant | Social · Vacant
       </h3>
 
       <div className="officer-mobile">
